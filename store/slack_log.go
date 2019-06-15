3b022c2ce260fece5275bdbe22eef7d655753a17
kyontan <kyontan@monora.me>
2019-06-16 07:06:28 +0900

    GetSlackLog divite into GetSlackLogFromCache and GetSlackLogFromAPI

--- store/slack_log.go
+++ store/slack_log.go
@@ -3,10 +3,12 @@ package store
 import (
 	"errors"
 	"strings"
+
+	"github.com/nlopes/slack"
 )
 
 var (
-	log = map[string]LogData{} // key: "workspace,timestamp"
+	log = map[string]LogData{} // key: "workspace,timestamp" // TODO: set limit?
 )
 
 type LogData struct {
@@ -15,11 +17,13 @@ type LogData struct {
 }
 
 var (
-	ErrSourceChannelNotFound = errors.New("source channel is not found")
+	ErrSlackLogNotFound     = errors.New("slack log is not found from cache")
+	ErrSlackHistoryNotFound = errors.New("message history couldn't retrieve from GetConversationHistory")
 )
 
 func SetSlackLog(workspace, timestamp, channelName, text string) {
 	// register post to kv
+	// XXX: timestamp is unique only in channel
 	k := strings.Join([]string{workspace, timestamp}, ",")
 
 	// TODO: gc
@@ -29,13 +33,36 @@ func SetSlackLog(workspace, timestamp, channelName, text string) {
 	}
 }
 
-func GetSlackLog(workspace, timestamp string) (*LogData, error) {
+func GetSlackLogFromCache(workspace, timestamp string) *LogData {
 	parent := strings.Join([]string{workspace, timestamp}, ",")
 	val, ok := log[parent]
-	if ok == false {
-		// TODO: if can't get channel name, search old message using slack API
-		return nil, ErrSourceChannelNotFound
+	if !ok {
+		return nil
+	}
+	return &val
+}
+
+func GetSlackLogFromAPI(api *slack.Client, workspace, channel, timestamp string) (*LogData, error) {
+	params := &slack.GetConversationHistoryParameters{
+		ChannelID: channel,
+		Latest:    timestamp,
+		Oldest:    timestamp,
+		Inclusive: true,
+		Limit:     1, // timestamp is guaranteed as unique (per channel)
 	}
 
-	return &val, nil
+	history, err := api.GetConversationHistory(params)
+	if err != nil {
+		return nil, err
+	}
+
+	if len(history.Messages) != 1 {
+		return nil, ErrSlackHistoryNotFound
+	}
+
+	msg := history.Messages[0]
+	return &LogData{
+		Channel: msg.Channel, // NOTE: this is not readable name
+		Body:    msg.Text,
+	}, nil
 }
